fc13f7f17126e790326a70468e07bb45c1dd904d
AagamanMainali35 <aagamanmainali35@gmail.com>
2025-03-27 07:29:29 +0545

    Created Notification Serilizer

--- ClassSphere/ClassApp/serializer.py
+++ ClassSphere/ClassApp/serializer.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from .models import Event,User,profile,PaymentStructure
+from .models import Event,User,profile,Notification
 
 class Eventserializer(serializers.ModelSerializer):
     class Meta:
         model=Event
         fields='__all__'
+
+
 class PaymentSerializer(serializers.Serializer):
     feeAmount = serializers.IntegerField() 
+
+
+class NotificationSerilizer(serializers.ModelSerializer):
+    class Meta:
+       model=Notification
+       fields = ['NotificationMsg']
