da9fc9ed7d6d1bede885b217112b438487240296
AagamanMainali35 <aagamanmainali35@gmail.com>
2025-04-14 15:03:49 +0545

    Added decorators.py and included role required Decooratoe for role based url restriction

---
+++ ClassSphere/ClassSphere/decorators.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponseForbidden
+from django.shortcuts import redirect
+from ClassApp.models import *
+
+def role_required(*required_roles):
+    def decorator(view_func):
+        def _wrapped_view(request, *args, **kwargs):
+            if not request.user.is_authenticated:
+                return redirect('login')
+
+            try:
+                profile_isntace = request.user.profile
+                if profile_isntace.role in required_roles:
+                    return view_func(request, *args, **kwargs)
+                else:
+                    return HttpResponseForbidden("You do not have permission to access this page.")
+            except profile.DoesNotExist:
+                return HttpResponseForbidden("Profile not found.")
+        return _wrapped_view
+    return decorator
